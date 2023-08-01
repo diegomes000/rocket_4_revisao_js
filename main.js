97cd05731842c0587b1e101c91a871f1f2994054
Diego Gomes <diegomes000@gmail.com>
2023-07-31 21:16:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log("Hello world!")
+
+let number1 = 10;
+let number2 = 2;
+
+console.log(number1+number2)
+
+console.log(!isNaN(number1))
+
+    if (!isNaN(number1)) {
+        console.log('É um número')
+    } else {
+        console.log('Não é um número')
+    }
+
+    if (typeof number2 === 'Number') {
+        console.log('É um número')
+    } else {
+        console.log('Não é um número')
+    }
+
+let word1 = "Gato";
+
+console.log(typeof word1)
+
+    if (typeof word1 == "string") {
+        console.log('É string')
+    } else {
+        console.log('Não é string')
+    }
+
+
+
